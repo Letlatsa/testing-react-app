1ed8ab6bfce082ff2fa658ed6b4f4107c31b865c
Letlatsa <leboletlatsa@gmail.com>
2024-08-01 16:49:31 +0200

    App tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,39 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen, waitFor } from "@testing-library/react";
+import fetchMock from "fetch-mock"; // Import fetch-mock
+import mockData from "./mockData";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+beforeEach(() => {
+  // Mock the API call
+  fetchMock.mock("https://jsonplaceholder.typicode.com/todos", {
+    status: 200,
+    body: mockData
+  });
+});
+
+afterEach(() => {
+  fetchMock.restore(); // Restore fetch after each test
+});
+
+describe("<App /> tests", () => {
+  it("renders <App />", async () => {
+    render(<App />);
+    
+    // Wait for the loading text to be removed
+    await waitFor(() => {
+      expect(screen.queryByText(/loading/i)).toBeNull();
+    });
+  });
+
+    it("should add a todo item", async () => {
+    fetchMock.once(
+      '/api/todo',
+      {
+        userId: 3,
+        id: Math.floor(Math.random() * 100) + 1,
+        title: "Do math homework",
+        completed: false
+      }
+    );});
 });
